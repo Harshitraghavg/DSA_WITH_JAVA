16889b8021f74c852e7627015f49685483455db7
Harshit Raghav <harshitraghav012@gmail.com>
2026-01-26 21:02:22 +0530

    add changed code

--- Array_MaxSum_SubArray.java
+++ Array_MaxSum_SubArray.java
@@ -1,31 +1,65 @@
 import java.util.*;
 public class Array_MaxSum_SubArray {
-    public static void PreFixSum(int arr[]){
+    public static void BruteForse_maxSum(int arr[]){            //time complexity=n^3
         int currSum=0;
-        int maxSum=Integer.MIN_VALUE;                                          //TIME COMPLICITY=N^2
-        int[] prefix=new int[arr.length];
+        int maxSum=Integer.MIN_VALUE;
+        
 
-        //calculate prefix
-        for(int i=1;i<prefix.length;i++){
-            prefix[i]=prefix[i-1]+arr[i];
-        }
-
-        //now find start and end value of arr
+        //this loop for find start element of the arr
         for(int i=0;i<arr.length;i++){
             int start=i;
+
+            //this loop for find end element of the arr
             for(int j=i;j<arr.length;j++){
                 int end=j;
+                currSum=0;
 
-                currSum=start==0 ?prefix[end] : prefix[end]-prefix[start-1];
-
+                //this loop for print the currSum of the arr
+                for(int k=start;k<end;k++){
+                    currSum +=arr[k];
+                }
+                System.out.println(currSum);
                 if(maxSum<currSum){
                     maxSum=currSum;
                 }
+
             }
         }
-
-        System.out.println("maxSum:"+maxSum);
+        System.out.println("Maximum Subarray Sum is: "+maxSum);
     }
+
+
+
+
+
+
+
+    // public static void PreFixSum(int arr[]){
+    //     int currSum=0;
+    //     int maxSum=Integer.MIN_VALUE;                                          //TIME COMPLICITY=N^2
+    //     int[] prefix=new int[arr.length];
+
+    //     //calculate prefix
+    //     for(int i=1;i<prefix.length;i++){
+    //         prefix[i]=prefix[i-1]+arr[i];
+    //     }
+
+    //     //now find start and end value of arr
+    //     for(int i=0;i<arr.length;i++){
+    //         int start=i;
+    //         for(int j=i;j<arr.length;j++){
+    //             int end=j;
+
+    //             currSum=start==0 ?prefix[end] : prefix[end]-prefix[start-1];
+
+    //             if(maxSum<currSum){
+    //                 maxSum=currSum;
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println("maxSum:"+maxSum);
+    // }
 
     // public static void KadansAlgorithm(int arr[]){                                      //+val + +val = +val
     //     int ms=Integer.MIN_VALUE;                                                      //Big_+val + small_-val = +val 
@@ -42,7 +76,8 @@ public class Array_MaxSum_SubArray {
     public static void main(String[] args){
         int arr[]={-2,-3,4,-1,-2,1,5,-3};
         //KadansAlgorithm(arr);
-        PreFixSum(arr);
+        //PreFixSum(arr);
+        BruteForse_maxSum(arr);
 
     }
 }
