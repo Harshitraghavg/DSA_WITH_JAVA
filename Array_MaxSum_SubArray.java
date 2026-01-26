6f3f60d9d5fa7ce151481bbd3889f95791637d87
Harshit Raghav <harshitraghav012@gmail.com>
2026-01-26 20:36:45 +0530

    file change

--- Array_MaxSum_SubArray.java
+++ Array_MaxSum_SubArray.java
@@ -1,21 +1,48 @@
 import java.util.*;
 public class Array_MaxSum_SubArray {
+    public static void PreFixSum(int arr[]){
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;                                          //TIME COMPLICITY=N^2
+        int[] prefix=new int[arr.length];
 
-    public static void KadansAlgorithm(int arr[]){                                      //+val + +val = +val
-        int ms=Integer.MIN_VALUE;                                                      //Big_+val + small_-val = +val 
-        int cs=0;                                                                       //Small_+val + big_-val = -val
-        for(int i=0;i<arr.length;i++){                                                  //-val + -val = -val
-            cs=cs+arr[i];
-            if(cs<0){                                                                    //time complexity = O(n)
-                cs=0;
-            }
-            ms=Math.max(cs,ms);
+        //calculate prefix
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
         }
-        System.out.println("Maximum Subarray Sum is: "+ms);
+
+        //now find start and end value of arr
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+
+                currSum=start==0 ?prefix[end] : prefix[end]-prefix[start-1];
+
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+
+        System.out.println("maxSum:"+maxSum);
     }
+
+    // public static void KadansAlgorithm(int arr[]){                                      //+val + +val = +val
+    //     int ms=Integer.MIN_VALUE;                                                      //Big_+val + small_-val = +val 
+    //     int cs=0;                                                                       //Small_+val + big_-val = -val
+    //     for(int i=0;i<arr.length;i++){                                                  //-val + -val = -val
+    //         cs=cs+arr[i];
+    //         if(cs<0){                                                                    //time complexity = O(n)
+    //             cs=0;
+    //         }
+    //         ms=Math.max(cs,ms);
+    //     }
+    //     System.out.println("Maximum Subarray Sum is: "+ms);
+    // }
     public static void main(String[] args){
         int arr[]={-2,-3,4,-1,-2,1,5,-3};
-        KadansAlgorithm(arr);
+        //KadansAlgorithm(arr);
+        PreFixSum(arr);
 
     }
 }
