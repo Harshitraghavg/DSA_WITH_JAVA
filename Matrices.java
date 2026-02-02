8df3d363966030260f1a53b7b612d0b215f7b0fb
Harshit Raghav <harshitraghav012@gmail.com>
2026-02-02 23:34:46 +0530

    day-11

---
+++ Matrices.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Matrices {
+
+    //search any element in the matrix
+    public static boolean search(int matrix[][] , int key){
+        for(int i=0; i<matrix.length;i++){          //travel row bais
+            for(int j=0; j<matrix[0].length; j++){      // travel colm bais
+                if(matrix[i][j]==key){
+                    System.out.println("found at cell (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        int matrix[][]=new int[3][3];
+        int n=matrix.length;   //no. of rows
+        int m=matrix[0].length; // no. of columns
+        
+        
+        //take matrix input
+        Scanner sc=new Scanner(System.in);
+        for(int i=0; i<n;i++){          //travel row bais
+            for(int j=0; j<m; j++){      // travel colm bais
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+
+        //output
+        for(int i=0; i<n;i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        search(matrix,8);
+
+    }
+}
